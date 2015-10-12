c64a595ae5a2c081123919c0d8fa9f01946ef6be
Lu <luxh93@gmail.com>
2015-10-12 17:59:23 -0400

    Autolayout fix and GraphVC

---
+++ Calculator/GraphView.swift
@@ -0,0 +1,13 @@
+//
+//  GraphView.swift
+//  Calculator
+//
+//  Created by Xing Hui Lu on 10/12/15.
+//  Copyright © 2015 Xing Hui Lu. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class GraphView: UIView {
+
+}
---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Xing Hui Lu on 10/12/15.
+//  Copyright © 2015 Xing Hui Lu. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    @IBOutlet weak var graphView: GraphView!
+
+}
